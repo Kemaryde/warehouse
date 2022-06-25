6a9f29bd832d66eafdb18c196ff38c50eeafa690
Johannes Losch <hi@jlosch.de>
2022-06-26 00:45:26 +0200

    springdoc config

--- src/main/java/de/kemary/warehouse/WarehouseApplication.java
+++ src/main/java/de/kemary/warehouse/WarehouseApplication.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
+@OpenAPIDefinition(servers = {@Server(url = "https://test.kemary.de/api/", description = "Default Server URL")})
 @SpringBootApplication
 public class WarehouseApplication {
 
